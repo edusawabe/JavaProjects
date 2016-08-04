78fd1b5a47a95f52501c2774316e51a701aaedd2
eduardo.sawabe <eduardo.sawabe@ed8b4a5b-6a15-8946-8520-854d1a4d267d>
2016-08-04 11:07:34 +0000

    Alteração para Tentativa de Execução via java

--- SVNExport/src/application/SVNExportController.java
+++ SVNExport/src/application/SVNExportController.java
@@ -2,9 +2,11 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -18,14 +20,19 @@ public class SVNExportController {
 	private Button btConfirm;
 
 	@FXML
-	private void processar(Event event){
+	private void processar(Event event) throws IOException{
 		String texto = taArea.getText();
 		String [] linhas = texto.split("\n");
 		String currentRevision = null;
 		String dir = null;
 		String res = new String();
 
-		res = "rmdir C:\\export /s /q \n";
+		//res = "rmdir C:\\export /s /q \n";
+		File exportDir = new File("C:\\exp");
+		if(exportDir.exists())
+			delete(exportDir);
+		exportDir.delete();
+		LinkedList<String[]> commandlist = new LinkedList<String[]>();
 		for (int i = 0; i < linhas.length; i++) {
 			if (linhas[i].contains("Revision: ")){
 				String [] tmp = linhas[i].split("Revision: ");
@@ -35,9 +42,21 @@ public class SVNExportController {
 				String [] tmp2 = linhas[i].split(" : ");
 				dir = tmp2[1].substring(0, tmp2[1].lastIndexOf(47));
 				dir = dir.replace('/', '\\');
-				dir = "\"" + "C:\\export" + dir + "\"";
-				res = res + "mkdir " + dir + " \n";
-				res = res + "svn export -r" + currentRevision + " --force \"https://scm-coconet.capgemini.com/svn/repos/br-cpmb-bd-pf-rfin" + tmp2[1] + "\" "+ dir + "\n";
+				//dir = "\"" + "C:\\exp" + dir + "\"";
+				//res = res + "mkdir " + dir + " \n";
+				dir = "C:\\exp" + dir;
+				File newDir = new File(dir);
+				newDir.mkdirs();
+				dir =  "\"" +  dir + "\"";
+				res = res + "C:\\Program Files\\TortoiseSVN\\bin\\svn.exe export -r" + currentRevision + " --force \"https://scm-coconet.capgemini.com/svn/repos/br-cpmb-bd-pf-rfin" + tmp2[1] + "\" "+ dir + "\n";
+				String[] toAdd = new String[6];
+				toAdd[0] = "svn";
+				toAdd[1] = "export";
+				toAdd[2] = "-r"+currentRevision;
+				toAdd[3] = "--force";
+				toAdd[4] = "\"https://scm-coconet.capgemini.com/svn/repos/br-cpmb-bd-pf-rfin" + tmp2[1] + "\" ";
+				toAdd[5] = dir;
+				commandlist.add(toAdd);
 			}
 		}
 
@@ -45,28 +64,27 @@ public class SVNExportController {
 			FileWriter writer = new FileWriter(new File("C:\\export.bat"));
 			writer.append(res);
 			writer.close();
-/*
+
 			Process process = null;
 			BufferedReader stdInput;
 			BufferedReader stdError;
 			String[] cmd = res.split("\n");
 			String content = new String();
 
-			for (int i = 0; i < cmd.length; i++) {
-				String[] toCall = new String[2];
-				toCall[0] = "C:\\Windows\\System32\\cmd.exe";
-				toCall[1] = cmd[i];
-				process = Runtime.getRuntime().exec(toCall);
+			for (int i = 0; i < commandlist.size(); i++) {
+				String[] toCall = cmd[i].split(" ");
+				process = Runtime.getRuntime().exec(commandlist.get(i));
+				//content = content + commandlist.get(i).toString()+ "\n";
 				// Get input streams
 				stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
 				stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
 				// Read command standard output
 				String s;
-				//content = content + "======> Outputs:" + "\n";
-				//while ((s = stdInput.readLine()) != null) {
-				//	content = content + s + "\n";
-				//}
+				content = content + "======> Outputs:" + "\n";
+				while ((s = stdInput.readLine()) != null) {
+					content = content + s + "\n";
+				}
 
 				content = content + "======> Erros:" + "\n";
 				// Read command errors
@@ -75,11 +93,20 @@ public class SVNExportController {
 				}
 				taArea.setText(content);
 			}
-*/
+
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		taArea.setText(res);
 	}
+
+	private void delete(File f) throws IOException {
+		  if (f.isDirectory()) {
+			    for (File c : f.listFiles())
+			      delete(c);
+			  }
+			  if (!f.delete())
+			    throw new FileNotFoundException("Failed to delete file: " + f);
+			}
 }
