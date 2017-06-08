33e54079e7a9fd76eddaea6ef1ec387e2f6e37ba
eduardo.sawabe <eduardo.sawabe@ed8b4a5b-6a15-8946-8520-854d1a4d267d>
2017-06-08 17:35:50 +0000

    Inicio Implementação Mesa 3

--- PokerTimerFX/src/application/PokerTimerFXController.java
+++ PokerTimerFX/src/application/PokerTimerFXController.java
@@ -136,6 +136,8 @@ public class PokerTimerFXController implements Initializable{
 	@FXML
 	private ListView<String> listMesa2;
 	@FXML
+	private ListView<String> listMesa3;
+	@FXML
 	private Button btAdicionaJogador;
 	@FXML
 	private Button btExcluirJogador;
@@ -175,6 +177,7 @@ public class PokerTimerFXController implements Initializable{
 	private ObservableList<String> oListComboJogador = FXCollections.observableArrayList();
 	private ObservableList<String> oListJogadoresMesa1 = FXCollections.observableArrayList();
 	private ObservableList<String> oListJogadoresMesa2 = FXCollections.observableArrayList();
+	private ObservableList<String> oListJogadoresMesa3 = FXCollections.observableArrayList();
 
 	private ObservableList<String> oListJogadoresBk = FXCollections.observableArrayList();
 	private ObservableList<String> oListRebuysBk = FXCollections.observableArrayList();
@@ -183,6 +186,7 @@ public class PokerTimerFXController implements Initializable{
 	private ObservableList<String> oListComboJogadorBk = FXCollections.observableArrayList();
 	private ObservableList<String> oListJogadoresMesa1Bk = FXCollections.observableArrayList();
 	private ObservableList<String> oListJogadoresMesa2Bk = FXCollections.observableArrayList();
+	private ObservableList<String> oListJogadoresMesa3Bk = FXCollections.observableArrayList();
 
     private boolean paused;
     private boolean play;
@@ -202,13 +206,13 @@ public class PokerTimerFXController implements Initializable{
     private double total5l = 0;
     private LinkedList<String> llMesa1;
     private LinkedList<String> llMesa2;
+    private LinkedList<String> llMesa3;
     private Timeline updateGuitask;
     private String lastKey;
     private String inputName;
     private int currentSelection;
     private int lastInput;
     private String lastSelectedName;
-    private int hidePainelInferior;
     private TimerWindowController timerWindowController;
 	private Stage primaryStageTimer;
 	private final static Logger logger = Logger.getLogger(PokerTimerFXController.class);
@@ -326,10 +330,12 @@ public class PokerTimerFXController implements Initializable{
 
 	@FXML
 	private void trocarJogadorMesa(Event evt) {
-		listMesa1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		listMesa2.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		listMesa1.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		listMesa2.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		listMesa3.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 		if (listMesa1.getSelectionModel().getSelectedIndex() < 0
-				&& listMesa2.getSelectionModel().getSelectedIndex() < 0) {
+				&& listMesa2.getSelectionModel().getSelectedIndex() < 0
+				&& listMesa3.getSelectionModel().getSelectedIndex() < 0) {
 
 			Alert alError = new Alert(AlertType.ERROR);
 			alError.setTitle("Selecionar Jogador");
@@ -676,6 +682,10 @@ public class PokerTimerFXController implements Initializable{
     	RankingController rankingController;
     	Resumo resumo = new Resumo();
     	boolean added;
+
+    	ExcelRankingGenerator excelRankingGenerator = new ExcelRankingGenerator();
+    	excelRankingGenerator.setlPlayer(lPlayer);
+    	excelRankingGenerator.generateExcel();
 
     	//obtem Loader
     	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Ranking.fxml"));
@@ -1680,7 +1690,7 @@ public class PokerTimerFXController implements Initializable{
         	statsMedia.setText("" + (((totalJogadores + totalRebuy) * 3000)/totalJogando));
 
         if(timerWindowController != null){
-	        timerWindowController.getLbTimer().setText(lbTimer.getText());
+	        timerWindowController.getLbTimer().setText(lbTimer.getText() + "\n" + "Média Fichas: " + statsMedia.getText());
 	        timerWindowController.getPbProgress().setProgress(timerBar.getProgress());
 			timerWindowController.getLbBlindsAtual().setText(
 					"Atual: " + smallAtual.getText() + "/" + bigAtual.getText() + " Ante: " + valorAnteAtual.getText());
@@ -1689,7 +1699,7 @@ public class PokerTimerFXController implements Initializable{
 			timerWindowController.getLbProxBreak().setText("Próximo Break: " + lbProximoBreak.getText());
 	        if(currentRound > Constants.LAST_BREAK_ROUND)
 	        	timerWindowController.getLbProxBreak().setText("Próximo Break: ");
-	        timerWindowController.getLbMediaFichas().setText("Média Fichas: " + statsMedia.getText());
+	        //timerWindowController.getLbMediaFichas().setText("Média Fichas: " + statsMedia.getText());
         }
         if(currentRound > Constants.LAST_BREAK_ROUND)
         	lbProximoBreak.setText("");
