2c0f1eae7b1acad664a08423dbde75d008f6f5a4
edusawabe <edusawabe@gmail.com>
2019-01-08 18:56:39 -0200

    Delete

--- CobolParserFXML/PokerTimerFX/build/build/src/model/BooleanCell.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import application.RebuysConsolidadoController;
-import javafx.scene.control.TableCell;
-
-public class BooleanCell extends TableCell<RebuysConsolidadoController, Boolean>{
-
-}
